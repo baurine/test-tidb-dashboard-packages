d0bd59dd04c1ced2acf72835f25aff03a757089a
baurine <2008.hbl@gmail.com>
2020-03-17 16:04:59 +0800

    auth

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { HashRouter as Router } from 'react-router-dom'
+import { StatementsOverviewPage } from '@pingcap-incubator/statement'
+import * as DashboardClient from '@pingcap-incubator/dashboard_client'
+import * as authUtil from './auth'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import 'antd/dist/antd.css'
+
+const dashboardClient = new DashboardClient.DefaultApi({
+  basePath: 'http://127.0.0.1:12333/dashboard/api',
+  apiKey: () => authUtil.getAuthTokenAsBearer()
+})
+
+dashboardClient
+  .userLoginPost({
+    username: 'root',
+    password: '',
+    is_tidb_auth: true
+  })
+  .then(r => authUtil.setAuthToken(r.data.token))
+
+const App = () => (
+  <Router>
+    <div style={{ margin: 12 }}>
+      <StatementsOverviewPage
+        dashboardClient={dashboardClient}
+        detailPagePath="/statement/detail"
+      />
     </div>
-  );
-}
+  </Router>
+)
 
-export default App;
+export default App
